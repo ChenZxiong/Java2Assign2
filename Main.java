71fe43180f4622a3dd8d3d680b47b7876d446c19
ChenZxiong <95575156+ChenZxiong@users.noreply.github.com>
2022-11-23 17:48:55 +0800

    Update for checkStyle

--- Main.java
+++ Main.java
@@ -1,31 +1,30 @@
+import java.net.Socket;
+import java.util.Scanner;
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
 import javafx.scene.layout.Pane;
 import javafx.stage.Stage;
 
-import java.net.Socket;
-import java.util.Scanner;
-
 public class Main extends Application {
     @Override
     public void start(Stage primaryStage) {
-        try {
-            FXMLLoader fxmlLoader = new FXMLLoader();
-            fxmlLoader.setLocation(getClass().getClassLoader().getResource("mainUI.fxml"));
-            Pane root = fxmlLoader.load();
-            Controller controller = fxmlLoader.getController();
-            Scanner in = new Scanner(System.in);
-            String name = in.next();
-            Socket socket1 = null;
-            Client client = new Client(socket1, socket1);
-            primaryStage.setTitle("Tic Tac Toe");
-            primaryStage.setScene(new Scene(root));
-            primaryStage.setResizable(false);
-            primaryStage.show();
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
+    try {
+      FXMLLoader fxmlLoader = new FXMLLoader();
+      fxmlLoader.setLocation(getClass().getClassLoader().getResource("mainUI.fxml"));
+      Pane root = fxmlLoader.load();
+      Controller controller = fxmlLoader.getController();
+      Scanner in = new Scanner(System.in);
+      String name = in.next();
+      Socket socket1 = null;
+      Client client = new Client(socket1, socket1);
+      primaryStage.setTitle("Tic Tac Toe");
+      primaryStage.setScene(new Scene(root));
+      primaryStage.setResizable(false);
+      primaryStage.show();
+    } catch (Exception e) {
+      e.printStackTrace();
+    }
     }
 
     public static void main(String[] args) {
