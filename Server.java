c46f6d6867bb7b6d39cb9c193c4825e7da286153
ChenZxiong <95575156+ChenZxiong@users.noreply.github.com>
2022-11-23 17:49:25 +0800

    Update for checkStyle

--- Server.java
+++ Server.java
@@ -5,24 +5,24 @@ import java.net.Socket;
 public class Server {
 
     public static void main(String[] args) {
-        ServerSocket serverSocket = null;
-        try {
-            serverSocket = new ServerSocket(8848);
-            Socket wait_socket = null;
-            System.out.println("Waiting player...");
-            while (true) {
-                Socket come = serverSocket.accept();
-                if (wait_socket == null) {
-                    wait_socket = come;
-                    System.out.println("A player enter, wait to match another player...");
-                } else {
-                    System.out.println("Match successfully, game start");
-                    new Client(wait_socket, come).start();
-                }
-            }
-        } catch (IOException e) {
+    ServerSocket serverSocket = null;
+    try {
+      serverSocket = new ServerSocket(8848);
+      Socket wait_socket = null;
+      System.out.println("Waiting player...");
+      while (true) {
+        Socket come = serverSocket.accept();
+        if (wait_socket == null) {
+          wait_socket = come;
+          System.out.println("A player enter, wait to match another player...");
+        } else {
+           System.out.println("Match successfully, game start");
+           new Client(wait_socket, come).start();
+        }
+      }
+    } catch (IOException e) {
             System.out.println("Error!");
             throw new RuntimeException(e);
-        }
+    }
     }
 }
